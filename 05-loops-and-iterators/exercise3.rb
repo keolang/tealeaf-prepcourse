ef67276706c36762867bab48dd5b348d8cd47196
Keola Ng <keola.ng@gmail.com>
2015-04-30 15:01:53 -1000

    added comments

--- 05-loops-and-iterators/exercise3.rb
+++ 05-loops-and-iterators/exercise3.rb
@@ -1,4 +1,5 @@
 ##
+# Exercise 3
 # Use the each_with_index method to iterate through an array of your creation 
 # that prints each index and value of the array
 ##
--- 05-loops-and-iterators/exercise4.rb
+++ 05-loops-and-iterators/exercise4.rb
@@ -1,4 +1,5 @@
 ##
+# Exercise 4
 # Write a method that counts down to zero using recursion.
 ##
 
